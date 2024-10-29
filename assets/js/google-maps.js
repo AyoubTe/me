8389fd8eb15516c672bc21343fc86e66d21d1f58
Ayoub SAMI <120687459+AyoubTe@users.noreply.github.com>
2024-10-29 20:41:50 +0100

    Location

--- assets/js/google-maps.js
+++ assets/js/google-maps.js
@@ -1,15 +1,15 @@
 function initMap() {
     // Latitude and Longitude
-    var myLatLng = {lat: -6.207690, lng: 106.985270};
+    var myLatLng = { lat: 43.6045, lng: 1.4442 };
 
     var map = new google.maps.Map(document.getElementById('google-maps'), {
-        zoom: 17,
+        zoom: 12,
         center: myLatLng
     });
 
     var marker = new google.maps.Marker({
         position: myLatLng,
         map: map,
-        title: 'South Jakarta, INA' // Title Location
+        title: 'Toulouse , France' // Title Location
     });
 }
